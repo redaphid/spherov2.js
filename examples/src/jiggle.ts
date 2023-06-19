b4461535d33cf8f58cdd5eddae608d4c207a1a2d
redaphid <iam@aaronherres/com>
2023-06-18 19:03:15 -0700

    jiggle this thang
    Co-authored-by: Roy van de Water <roy.v.water@gmail.com>

---
+++ examples/src/jiggle.ts
@@ -0,0 +1,22 @@
+import { SpheroMini, Utils, Event } from 'spherov2.js';
+import { starter } from './utils/starter';
+// SORRY FOR THIS CODE, It is my playground for now
+let heading = 0
+const cmdPlay = (toy: SpheroMini) => {
+  toy.configureCollisionDetection();
+  toy.on(Event.onCollision, (e) => {
+    console.log('COLLISION', e);
+  })
+  const loop = async () => {
+    while (true) {
+      heading += 10
+      heading = heading % 360
+      toy.roll(0.1, heading, [])
+      await Utils.wait(100);
+    }
+  }
+
+  loop();
+};
+
+starter(cmdPlay);
