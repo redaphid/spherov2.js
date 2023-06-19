2046b3a26b739b92d4626d7ad9ce5d663e81e59a
redaphid <iam@aaronherres/com>
2023-06-18 21:55:37 -0700

    simplify cmd.ts example. Maybe it works, maybe it doesn't. I can't find the fucking shell to mine rn.

--- examples/src/cmd.ts
+++ examples/src/cmd.ts
@@ -1,8 +1,8 @@
-import { SpheroMini, Utils, Event } from '../../lib';
+import { SpheroMini, Event } from '../../lib';
 import { emitKeypressEvents } from 'readline';
 import { starter } from './utils/starter';
-// SORRY FOR THIS CODE, It is my playground for now
-const cmdPlay = (toy: SpheroMini) => {
+
+const start = (toy: SpheroMini) => {
   let pressTimeout: NodeJS.Timer;
   let heading = 0;
   let currentSpeed = 0;
@@ -10,97 +10,87 @@ const cmdPlay = (toy: SpheroMini) => {
   let executing = true;
   let calibrating = false;
   let offset = 0;
-  let health = 100;
-  toy.configureCollisionDetection();
+
+  toy.configureSensorStream();
+
   const cancelPress = () => {
     clearTimeout(pressTimeout);
     pressTimeout = null;
   };
-  // a function that takes health from 0-100 and returns a gradient from green to red
-  const setHealthLight = (health: number) => {
-    const red = Math.floor(255 * (health / 100))
-    const green = Math.floor(255 * ((100 - health) / 100))
-    toy.setMainLedColor(red, green, 0)
-  }
-  setHealthLight(health)
+
   const addTimeout = () => {
     pressTimeout = setTimeout(() => {
       currentSpeed = 0;
     }, 500);
   };
-  toy.on(Event.onCollision, () => {
-    health-=10
-    console.log({health})
-    console.log('COLLISION');
-    setHealthLight(health)
-  })
-  const loop = async () => {
-    // eslint-disable-next-line no-constant-condition
-    while (true) {
-      if (executing) {
-        toy.roll(
-          currentSpeed,
-          calibrating ? heading : (heading + offset) % 360,
-          []
-        );
-      }
-      if (currentSpeed === 0 && !calibrating) {
-        executing = false;
-      }
-      if (calibrating) {
-        heading += 5;
 
-        if (heading > 360) {
-          heading = 0;
-        }
-      }
-      await Utils.wait(100);
+  const loop = async () => {
+    if (executing) {
+      toy.roll(
+        currentSpeed,
+        calibrating ? heading : (heading + offset) % 360,
+        []
+      );
+    }
+    if (currentSpeed === 0 && !calibrating) {
+      executing = false;
+    }
+    if (calibrating) {
+      heading += 5;
+      heading%=360
     }
   };
 
-  const handle = async (key = '', symbol: { name?: string } = {}) => {
+  const onKeyPress = async (key = '', symbol: { name?: string } = {}) => {
     cancelPress();
-    if (symbol.name === 'up') {
-      heading = 0;
-      currentSpeed = speed;
-      executing = true;
-      addTimeout();
-    } else if (symbol.name === 'left') {
-      heading = 270;
-      currentSpeed = speed;
-      executing = true;
-      addTimeout();
-    } else if (symbol.name === 'right') {
-      heading = 90;
-      currentSpeed = speed;
-      executing = true;
-      addTimeout();
-    } else if (symbol.name === 'down') {
-      heading = 180;
-      currentSpeed = speed;
-      executing = true;
-      addTimeout();
-    }
-
-    if (key === 'q') {
-      speed += 10;
-      // console.log('speed', speed);
-    } else if (key === 'z') {
-      speed -= 10;
-      // console.log('speed', speed);
-    } else if (key === 'p') {
-      process.exit();
-    } else if (key === 's') {
-      toy.sleep();
-    } else if (key === 'a') {
-      toy.wake();
-    } else if (key === 'c') {
-      if (calibrating) {
-        calibrating = false;
-        await toy.setBackLedIntensity(0);
-        offset = heading;
+    const keyToActionMap = {
+      up: async () => {
         heading = 0;
-      } else {
+        currentSpeed = speed;
+        executing = true;
+        addTimeout();
+      },
+      left: async () => {
+        heading = 270;
+        currentSpeed = speed;
+        executing = true;
+        addTimeout();
+      },
+      right: async () => {
+        heading = 90;
+        currentSpeed = speed;
+        executing = true;
+        addTimeout();
+      },
+      down: async () => {
+        heading = 180;
+        currentSpeed = speed;
+        executing = true;
+        addTimeout();
+      },
+      q: async () => {
+        speed += 10;
+      },
+      z: async () => {
+        speed -= 10;
+      },
+      p: async () => {
+        process.exit();
+      },
+      s: async () => {
+        toy.sleep();
+      },
+      a: async () => {
+        toy.wake();
+      },
+      c: async () => {
+        if (calibrating) {
+          calibrating = false;
+          await toy.setBackLedIntensity(0);
+          offset = heading;
+          heading = 0;
+          return;
+        }
         await toy.setBackLedIntensity(255);
         currentSpeed = 0;
         executing = true;
@@ -108,13 +98,12 @@ const cmdPlay = (toy: SpheroMini) => {
         calibrating = true;
       }
     }
+    if (keyToActionMap[key]) keyToActionMap[key]();
   };
-
   emitKeypressEvents(process.stdin);
   process.stdin.setRawMode(true);
-  process.stdin.on('keypress', handle);
-
-  loop();
+  process.stdin.on('keypress', onKeyPress);
+  toy.on(Event.onSensor,loop)
 };
 
-starter(cmdPlay);
+starter(start);
