e6e80c10b7a6e2daec4d0f8f6c883cc03e635fe0
redaphid <iam@aaronherres/com>
2023-06-19 00:22:18 -0700

    lotsa prey stuff

--- examples/src/jiggle.ts
+++ examples/src/jiggle.ts
@@ -3,33 +3,38 @@ import { starter } from './utils/starter';
 // SORRY FOR THIS CODE, It is my playground for now
 let heading = 0
 let speed = 0
-const cmdPlay = (toy: SpheroMini) => {
-  toy.configureCollisionDetection();
+const cmdPlay = async (toy: SpheroMini) => {
+  await toy.configureCollisionDetection();
   let waitTime = 10
+  let timeSinceLastCollision = 9999
   toy.on(Event.onCollision, (e) => {
     speed+=500 // if we collide, speed up by 500. this will switch us to "move mode"
-
+    heading += 180 // and turn around
     console.log('collide')
+    timeSinceLastCollision = 0
+    // turn the led red
+    toy.setMainLedColor(255, 0, 0)
   })
   const loop = async () => {
+    timeSinceLastCollision += waitTime
     while (true) {
       heading += 50 // rotate 50 degrees per interval
       waitTime = 10
-      speed -= 1 // slow down by 1
-      if(Math.random() > 0.99) speed=100 // randomly start moving 1% of the time
-      if(speed > 0){
-        // if we're in "move mode", then wait longer, letting the ball move further
+      if(Math.random() > 0.995) speed=200 // randomly start moving 1% of the time
+      if(speed > 0) {
+        speed -= 1 // slow down by 1
         waitTime = 30
-        heading = heading + 100
+        heading = ( heading + Math.random() * 100 ) % 360 // randomly change direction
+        if(timeSinceLastCollision < 100) continue
+        // turn the led green
+      } else {
+        if(timeSinceLastCollision < 100) continue
+        // turn the led blue
+        toy.setMainLedColor(0, 0, 255)
       }
-      speed < 0 ? speed = 0 : speed //make sure speed is never negative
       heading = heading % 360 // keep heading between 0 and 360
       toy.roll(speed, heading, [])
-      try{
       await Utils.wait(waitTime)
-      }catch(e){
-        console.error(e)
-      }
     }
   }
 
