d323966558cdf792707ade14886a23848151638e
redaphid <iam@aaronherres/com>
2023-06-21 11:54:20 -0700

    put shitty controls in to interact w/cat

--- examples/src/puck-1.ts
+++ examples/src/puck-1.ts
@@ -1,8 +1,13 @@
+
+import { emitKeypressEvents } from 'readline';
+import { stdin } from 'process';
+
 import { SpheroMini, Event } from '../../lib'
 import { starter } from './utils/starter'
 const timeout = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 const cmdPlay = async (toy: SpheroMini) => {
   let waitTime = 1
+  let flashlight = false
   const collisionTimeout = 100
   let timeSinceLastCollision = 9999
   let heading = 0;
@@ -13,15 +18,15 @@ const cmdPlay = async (toy: SpheroMini) => {
   const loop = async () => {
     try {
       timeSinceLastCollision += waitTime
-      if(cooldown > 0) {
+      if (cooldown > 0) {
         cooldown -= waitTime
         await timeout(waitTime)
         loop()
         return
       }
       if (speed > 0) { // move mode
-        console.log({speed})
-        speed -= 1
+        console.log({ speed })
+        speed -= waitTime
         if (timeSinceLastCollision > collisionTimeout) toy.setMainLedColor(0, 255, 0) // show green (move mode)
       } else {
         if (timeSinceLastCollision > collisionTimeout) {
@@ -34,12 +39,12 @@ const cmdPlay = async (toy: SpheroMini) => {
       }
 
       if (timeSinceLastCollision > collisionTimeout) heading += Math.random() * 10 // jiggle if we haven't collided recently
-      if(Math.random() < 0.01) heading += 180 // randomly turn around some of the time
+      if (Math.random() < 0.01) heading += 200 // randomly turn around some of the time
       heading = heading % 360 // keep heading between 0 and 360
 
       if (Math.random() < 0.01) speed = 25 // randomly start moving some of the time
-      let speedToGo = speed > 0 ? speed * 10+ 100: 0 // if we weren't moving, don't move
-      if(timeSinceLastCollision < collisionTimeout) speedToGo = 1000 // if we've collided recently, stop
+      let speedToGo = speed > 0 ? speed * 10 + 100 : 0 // if we weren't moving, don't move
+      if (timeSinceLastCollision < collisionTimeout) speedToGo = 1000 // if we've collided recently, go fast
       await toy.roll(speedToGo, heading, [])
     } catch (e) {
       console.log(e);
@@ -59,6 +64,42 @@ const cmdPlay = async (toy: SpheroMini) => {
     console.log('COLLISION');
     toy.setMainLedColor(255, 0, 0)
   })
+
+  // try to "sort of" control the sphere with the keyboard
+  const intervalTimes = (time: number, fn: () => void) => {
+    if(time <= 0) return
+    fn()
+    setTimeout(() => intervalTimes(time-1, fn), time-1)
+  }
+  stdin.setRawMode(true)
+  emitKeypressEvents(stdin)
+  // suppress keypresses from being printed to the terminal
+  stdin.on('keypress', (ch, {name:key, ctrl}) => {
+    const keyToActionMap = {
+      c: () => {
+        if (ctrl) process.exit()
+        // try to stop other behaviors
+        cooldown += 5000
+      },
+      w: () => speed += 100,
+      s: () => speed -= 100,
+      a: () => heading -= 90,
+      d: () => heading += 90,
+      e: () => intervalTimes(100, ()=> heading += 90),
+      q: () => timeSinceLastCollision = 0,
+      f: () =>  {
+        if(flashlight) {
+          toy.setBackLedIntensity(0)
+        } else {
+          toy.setBackLedIntensity(255)
+        }
+        flashlight = !flashlight
+      }
+
+    }
+    if (keyToActionMap[key]) keyToActionMap[key]()
+  })
+
   loop();
 };
 
